13c842867f1ced3b091e9652c8f048f5d9641626
jwellington58 <wj070996@gmail.com>
2018-03-12 11:31:30 -0300

    Adição dos models

---
+++ WebApiVendas/Models/Categoria.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace WebApiVendas.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new HashSet<Produto>();
+        }
+        public int Id {get; set;}
+        public int Descricao {get;set;}
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ WebApiVendas/Models/DetalhesPed.cs
@@ -0,0 +1,14 @@
+namespace WebApiVendas.Models
+{
+    public class DetalhesPed
+    {
+        public int ProdutoId { get; set; }
+        public int PedidoId { get; set; }
+        public double Preco { get; set; }
+        public double Desconto { get; set; }
+        public int Quantidade { get; set; }
+        public Produto Produto { get; set; }
+        public Pedido Pedido { get; set; }
+    }
+
+}
