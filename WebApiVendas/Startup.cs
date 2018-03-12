92435f8c00f4c9e2ca5860531d3a1e16eb054c42
jwellington58 <wj070996@gmail.com>
2018-03-12 13:28:26 -0300

    Adição do Context e das Migrations

--- WebApiVendas/Startup.cs
+++ WebApiVendas/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiVendas.Data.Context;
 
 namespace WebApiVendas
 {
@@ -24,6 +26,7 @@ namespace WebApiVendas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
